71b0dff180e46f45b442e3da902412383afdb232
nfseixas <nataliafseixas@hotmail.com>
2016-03-11 15:41:25 -0300

    Inclusão do método somar

---
+++ Calculadora/src/br/fatec/serviço/Calculadora.java
@@ -0,0 +1,19 @@
+package br.fatec.serviço;
+
+public class Calculadora {
+
+	public int soma(String parcelas){
+		int resultado = 0;
+		String[] vetorDeNumeros = parcelas.split(",");
+		
+		for(String numero: vetorDeNumeros){
+			if(!numero.trim().isEmpty()){
+				resultado += Integer.parseInt(numero);
+			}
+			
+		}
+		return resultado;
+		
+	}
+}
+
---
+++ Calculadora/src/br/fatec/teste/UC01SomaDeParcelas.java
@@ -0,0 +1,39 @@
+package br.fatec.teste;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.fatec.serviço.Calculadora;
+
+public class UC01SomaDeParcelas {
+
+	static Calculadora calculadora;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+				calculadora = new Calculadora();
+			}
+
+	@Test
+	public void CT01UC01SomaDeParcelas_com_sucesso(){
+		assertEquals("resultado=", 5, calculadora.soma("3,2"));
+	}
+	
+	@Test(expected= RuntimeException.class)
+	public void CT02UC01SomaDeParcelas_dados_invalidos(){
+		assertEquals("resultado=", 5, calculadora.soma("x,2"));
+	}
+	
+	
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+
+}
